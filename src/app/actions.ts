a531bfa5ffe325139347439531f548d635cd1690
Shubhranka <shubhrankavarma@gmail.com>
2024-10-23 18:32:07 +0530

    added handle price function

---
+++ src/app/actions.ts
@@ -0,0 +1,18 @@
+export async function handleCost(weekdays:number, weekends:number, sadhaks:number){
+    const days = weekdays + weekends
+    let price = 0;
+    const oneWeekendCharge = 250;
+    const twoWeekendCharge = 400;
+    switch (days) {
+        case 1: price = 2000; break;
+        case 2: price = 3500; break;
+        case 3: price = 4500; break;
+        case 4: price = 5000; break;
+        case 5: price = 5500; break;
+        case 6: price = 6000; break;
+        case 7: price = 6500; break
+    }
+    price += oneWeekendCharge * weekends + twoWeekendCharge * Math.floor(weekends / 2);
+    price = (Math.floor(sadhaks/2)+1)/sadhaks * price;
+    return price;
+}
