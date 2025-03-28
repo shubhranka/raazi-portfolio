d4b1ba2333b3eeee87a7dc3f0a0601f93cc86dee
Shubhranka <shubhrankavarma@gmail.com>
2025-03-28 20:44:35 +0530

    cleaned up

--- src/app/callback/payment_failed.tsx
+++ src/app/callback/payment_failed.tsx
@@ -1,6 +1,4 @@
-import Link from 'next/link'
-import { Button } from "@/components/ui/button"
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
 import { AlertCircle } from "lucide-react"
 
 interface PaymentFailedProps {
